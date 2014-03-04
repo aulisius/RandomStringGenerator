e2d716d8ae86c71c8d9fcbb54db35bc0705c265e
faizaanceg <nisarfaizaan@gmail.com>
2014-03-04 05:39:27 +0530

    updated

--- file.cpp
+++ file.cpp
@@ -1,18 +1,19 @@
 #include<chrono>
 #include<iostream>
 #include<random>
-#include"func.cpp"
+#include"functions.cpp"
 int main()
 {
-        str s("RAMAN"),s1;
-        str q(s1);
-        str r,a;
-        r=q+s1;
-        r.countocc();
-        r.disp();
-	a.randomizer();
-	a.countocc();
-	a.disp();
+        str a("RAMAN"); // Invokes the constructor
+	str b;
+        a.countocc(); //Counts the occurunces
+        a.disp(); // To display the data
+	a.randomizer(); // Generates new random string
+	a.countocc(); // Counts the occurunces
+	a.disp(); // To display
+	b.getdata(); // Get new data
+	b.cal(); //Calculates the strlen
+	b.anagram(); // To generate anagrams
         return 0;
 
 }
