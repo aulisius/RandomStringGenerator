3aceba903bffa150b6a677127986bb66f19d98c9
Faizaan <aulisius7@gmail.com>
2015-04-11 19:33:28 +0530

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,20 +1,14 @@
-#include<chrono>
-#include<iostream>
-#include<random>
-#include"functions.cpp"
+#include <chrono>
+#include <iostream>
+#include <random>
+#include "randstr.h"
+
 int main()
 {
-        str a("RAMAN"); // Invokes the constructor
-	str b;
-        a.countocc(); //Counts the occurunces
-        a.disp(); // To display the data
-	a.randomizer(); // Generates new random string
-	a.countocc(); // Counts the occurunces
-	a.disp(); // To display
-	b.getdata(); // Get new data
-	b.cal(); //Calculates the strlen
-	b.anagram(); // To generate anagrams
-        return 0;
+    str myString; // Invokes the constructor
+    myString.randomizer();
+    myString.display(); // To display
 
+    return 0;
 }
 
